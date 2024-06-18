197b04e9b62625a14523b4676fa9504b3896035d
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-06-17 21:58:44 -0400

    make betterer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ProjectsPage from './pages/ProjectsPage';
-import CriteriaConfigPage from './pages/CriteriaConfigPage';
-import CalculationPage from './pages/CalculationPage';
+import LandingPage from './pages/LandingPage';
+import Criteria from './pages/Criteria';
+import Rating from './pages/Rating';
+import Nav from './components/Nav';
 import './index.css';
 
 const App = () => {
   return (
     <Router>
+    <Nav />
       <div className="App">
         <h1>House Rating App</h1>
         <Routes>
-          <Route path="/" element={<ProjectsPage />} />
-          <Route path="/project/:id/configure" element={<CriteriaConfigPage />} />
-          <Route path="/project/:id/calculate" element={<CalculationPage />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/house-criteria" element={<Criteria />} />
+          <Route path="/house-ratings" element={<Rating />} />
         </Routes>
       </div>
     </Router>
