b348b496ed0431bef583bfdb92a19595cb4e4dae
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-06-18 21:38:50 -0400

    testing

--- backend/functions/house-hunting-authorizer/go.mod
+++ backend/functions/house-hunting-authorizer/go.mod
@@ -1,3 +1,5 @@
 module github.com/hnucamendi/house-hunting/backend/functions/house-hunting-authorizer
 
 go 1.22.3
+
+require github.com/aws/aws-lambda-go v1.47.0
--- backend/functions/house-hunting-authorizer/main.go
+++ backend/functions/house-hunting-authorizer/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func generateAllow() *events.APIGatewayV2CustomAuthorizerSimpleResponse {
+	return &events.APIGatewayV2CustomAuthorizerSimpleResponse{
+		IsAuthorized: true,
+	}
+}
+
+func HandleRequest(events *events.APIGatewayV2HTTPRequest) (*events.APIGatewayV2CustomAuthorizerSimpleResponse, error) {
+	fmt.Println("Hello, World!")
+	eventJson, _ := json.Marshal(events)
+	fmt.Println(events)
+	fmt.Println(eventJson)
+
+	lr := generateAllow()
+
+	return lr, nil
+
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	lambda.Start(HandleRequest)
 }
