00db3d17ecfe32c1c3f9dbac66f1a5d66faeb4f3
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-06-18 22:22:26 -0400

    saving

--- backend/functions/house-hunting-authorizer/main.go
+++ backend/functions/house-hunting-authorizer/main.go
@@ -8,13 +8,25 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+func generateAllow() *events.APIGatewayV2CustomAuthorizerSimpleResponse {
+	return &events.APIGatewayV2CustomAuthorizerSimpleResponse{
+		IsAuthorized: true,
+	}
+}
+
+func generateDeny() *events.APIGatewayV2CustomAuthorizerSimpleResponse {
+	return &events.APIGatewayV2CustomAuthorizerSimpleResponse{
+		IsAuthorized: false,
+	}
+}
+
 func HandleRequest(events *events.APIGatewayV2HTTPRequest) (*events.APIGatewayV2CustomAuthorizerSimpleResponse, error) {
 	fmt.Println("Hello, World!")
 	eventJson, _ := json.Marshal(events)
 	fmt.Println(events.RequestContext.Authentication)
 	fmt.Printf("HERE TAMOCHIM %s\n", eventJson)
 
-	lr := lambdautils.GenerateAllow()
+	lr := generateAllow()
 	return lr, nil
 
 }
--- backend/functions/house-hunting-projects/main.go
+++ backend/functions/house-hunting-projects/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func HandleRequest(event *events.APIGatewayV2HTTPRequest) (*events.APIGatewayV2HTTPResponse, error) {
-	return &events.APIGatewayV2HTTPResponse{}, nil
+	eventJson, _ := json.Marshal(event)
+	return &events.APIGatewayV2HTTPResponse{
+		StatusCode: 200,
+		Body:       fmt.Sprintf("Hello, World! this is a test for house-hunting-projects\n%s\n", eventJson),
+	}, nil
 
 }
 
