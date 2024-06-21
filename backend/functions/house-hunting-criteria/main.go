354dcfd220195020bb598842f0ce98f337029045
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-06-20 21:02:08 -0400

    save

--- backend/functions/house-hunting-criteria/main.go
+++ backend/functions/house-hunting-criteria/main.go
@@ -1,28 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func generateAllow() *events.APIGatewayV2CustomAuthorizerSimpleResponse {
-	return &events.APIGatewayV2CustomAuthorizerSimpleResponse{
-		IsAuthorized: true,
-	}
-}
-
-func HandleRequest(event *events.APIGatewayV2HTTPRequest) (*events.APIGatewayV2CustomAuthorizerSimpleResponse, error) {
-	fmt.Println("Hello, World!")
-	eventJson, _ := json.Marshal(event)
-	fmt.Printf("HERE TAMOCHIM %s\n", eventJson)
-
-	lr := generateAllow()
-
-	return lr, nil
-
+func HandleRequest() {
+	fmt.Println("Hello World")
 }
 
 func main() {
