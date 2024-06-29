855f92d30e949529823fd1530eaf8df7835573a9
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-06-29 18:19:01 -0400

    reformatting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
+import LandingPage from './pages/ProjectsPage';
 import Criteria from './pages/Criteria';
 import Rating from './pages/Rating';
 import Nav from './components/Nav';
@@ -8,10 +8,7 @@ import './index.css';
 const App = () => {
   return (
     <Router>
-    <Nav />
       <div className="App">
-        <h1>House Rating App</h1>
-        <h1>House Rating App</h1>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/house-criteria" element={<Criteria />} />
