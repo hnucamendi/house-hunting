d0b319de1bda6045d8925d10ff3d3d382064cd49
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-06-30 18:19:10 -0400

    adding auth

--- backend/functions/house-hunting-authorizer/main.go
+++ backend/functions/house-hunting-authorizer/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"os"
+	"log"
+	"strings"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
+
+type JWTPayload struct {
+	Iss             string `json:"iss"`
+	Sub             string `json:"sub"`
+	Aud             string `json:"aud"`
+	Exp             int64  `json:"exp"`
+	Iat             int64  `json:"iat"`
+	Jti             string `json:"jti"`
+	EmailVerified   bool   `json:"email_verified"`
+	CognitoUsername string `json:"cognito:username"`
+	OriginJti       string `json:"origin_jti"`
+	EventID         string `json:"event_id"`
+	TokenUse        string `json:"token_use"`
+	AuthTime        int64  `json:"auth_time"`
+	Email           string `json:"email"`
+}
 
 func generateAllow() *events.APIGatewayV2CustomAuthorizerSimpleResponse {
 	return &events.APIGatewayV2CustomAuthorizerSimpleResponse{
@@ -15,32 +34,65 @@ func generateAllow() *events.APIGatewayV2CustomAuthorizerSimpleResponse {
 	}
 }
 
+func generateDeny() *events.APIGatewayV2CustomAuthorizerSimpleResponse {
+	return &events.APIGatewayV2CustomAuthorizerSimpleResponse{
+		IsAuthorized: true,
+	}
+}
+
+func decodeSegment(seg string) ([]byte, error) {
+	// Add padding if necessary
+	if l := len(seg) % 4; l != 0 {
+		seg += strings.Repeat("=", 4-l)
+	}
+	return base64.URLEncoding.DecodeString(seg)
+}
+
+func parseJWT(token string) (JWTPayload, error) {
+	parts := strings.Split(token, ".")
+	if len(parts) != 3 {
+		return JWTPayload{}, fmt.Errorf("token contains an invalid number of segments")
+	}
+
+	payloadBytes, err := decodeSegment(parts[1])
+	if err != nil {
+		return JWTPayload{}, fmt.Errorf("failed to decode JWT payload: %v", err)
+	}
+	var payload JWTPayload
+	if err := json.Unmarshal(payloadBytes, &payload); err != nil {
+		return JWTPayload{}, fmt.Errorf("failed to unmarshal JWT payload: %v", err)
+	}
+
+	return payload, nil
+}
+
 func HandleRequest(event *events.APIGatewayV2HTTPRequest) (*events.APIGatewayV2CustomAuthorizerSimpleResponse, error) {
-	eventJson, err := json.Marshal(event)
+	a, f := strings.CutPrefix(event.Headers["authorization"], "Bearer ")
+	if !f {
+		log.Println("Authorization header malformed")
+		return generateDeny(), nil
+	}
+
+	fmt.Printf("Header JSON: %s\n", a)
+
+	p, err := parseJWT(a)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to marshal event: %s\n", err)
-		return generateAllow(), nil
+		log.Printf("Failed to parse JWT: %v\n", err)
+		return generateDeny(), nil
 	}
 
-	headerJson, err := json.Marshal(event.Headers)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to marshal event: %s\n", err)
-		return generateAllow(), nil
+	fmt.Printf("Parsed JWT: %v\n", p)
+
+	if p.Exp < time.Now().Unix() {
+		log.Println("Token expired")
+		return generateDeny(), nil
 	}
 
-	fmt.Println("Authorization Header:", event.Headers["authorization"])
-	fmt.Printf("Header JSON: %s\n", headerJson)
-	fmt.Printf("Event JSON: %s\n", eventJson)
-
-	// Add your custom authorization logic here
-	// Example: Check if the Authorization header is present
-	authHeader := event.Headers["authorization"]
-	if authHeader == "" {
-		fmt.Println("Authorization header missing")
-		return generateAllow(), nil
+	if !p.EmailVerified {
+		log.Println("Email not verified")
+		return generateDeny(), nil
 	}
 
-	// If the authorization is successful
 	return generateAllow(), nil
 }
 
--- frontend/src/pages/ProjectsPage.jsx
+++ frontend/src/pages/ProjectsPage.jsx
@@ -1,15 +1,32 @@
+import { useEffect, useState } from 'react';
 import Project from '../components/Project';
 import "../styles/pages/projectsPage.css";
 
+
 const LandingPage = () => {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    fetch('http://api.hnucamendi.net/projects', {
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${sessionStorage.getItem('idToken')}`
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => setProjects(data));
+  }, [])
+
   return (
     <div className="projects-page">
       <h1>Your Projects</h1>
-      <Project>
-        <h2>Title</h2>
-        <p>Description</p>
-        <button>Select</button>
-      </Project>
+      {projects.map((project) => (
+        <Project key={project.id}>
+          <h2>{project.title}</h2>
+          <p>{project.description}</p>
+          <button>Select</button>
+        </Project>
+      ))}
     </div>
   );
 };
