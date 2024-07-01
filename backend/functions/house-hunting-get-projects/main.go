d3184b30d7f0bc3448d4641308d6fbe4e9b3c501
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-07-01 09:56:50 -0400

    testing

--- backend/functions/house-hunting-get-projects/main.go
+++ backend/functions/house-hunting-get-projects/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -9,6 +10,49 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
+
+type StringValue struct {
+	S string `json:"S"`
+}
+
+type NumberValue struct {
+	N string `json:"N"`
+}
+
+type Note struct {
+	ID    StringValue `json:"id"`
+	Title StringValue `json:"title"`
+	Note  StringValue `json:"note"`
+}
+
+type Score struct {
+	ID    StringValue `json:"id"`
+	Title StringValue `json:"title"`
+	Score NumberValue `json:"score"`
+}
+
+type HouseEntry struct {
+	ID      StringValue `json:"id"`
+	Address StringValue `json:"address"`
+	Scores  []struct {
+		M Score `json:"M"`
+	} `json:"scores"`
+	Notes []struct {
+		M Note `json:"M"`
+	} `json:"notes"`
+}
+
+type Item struct {
+	ID           StringValue `json:"id"`
+	ProjectID    StringValue `json:"project_id"`
+	HouseEntries []struct {
+		M HouseEntry `json:"M"`
+	} `json:"house_entries"`
+}
+
+type Response struct {
+	Item Item `json:"Item"`
+}
 
 var sess = session.Must(session.NewSession())
 
@@ -48,9 +92,28 @@ func HandleRequest(event *events.APIGatewayV2HTTPRequest) (*events.APIGatewayV2H
 		}, nil
 	}
 
+	var response Response
+	err = json.Unmarshal([]byte(out.GoString()), &response)
+	if err != nil {
+		fmt.Println("Error unmarshaling response:", err)
+		return &events.APIGatewayV2HTTPResponse{
+			StatusCode: 500,
+			Body:       fmt.Sprintf("Failed to unmarshal response: %v", err),
+		}, nil
+	}
+
+	responseJSON, err := json.Marshal(response)
+	if err != nil {
+		fmt.Println("Error marshaling response:", err)
+		return &events.APIGatewayV2HTTPResponse{
+			StatusCode: 500,
+			Body:       fmt.Sprintf("Failed to marshal response: %v", err),
+		}, nil
+	}
+
 	return &events.APIGatewayV2HTTPResponse{
 		StatusCode: 200,
-		Body:       out.GoString(),
+		Body:       string(responseJSON),
 	}, nil
 
 }
