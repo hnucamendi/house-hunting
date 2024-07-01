76c70e0b9ffaeffd85bd951006738e04fd161c01
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-07-01 19:17:01 -0400

    testing

--- backend/functions/house-hunting-get-projects/main.go
+++ backend/functions/house-hunting-get-projects/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -10,55 +9,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
-
-type Response struct {
-	Item struct {
-		UserId       string `json:"id"`
-		ProjectId    string `json:"project_id"`
-		HouseEntries struct {
-			L []struct {
-				M struct {
-					Address struct {
-						S string `json:"S"`
-					} `json:"address"`
-					Id struct {
-						S string `json:"S"`
-					} `json:"id"`
-					Notes struct {
-						L []struct {
-							M struct {
-								Title struct {
-									S string `json:"S"`
-								} `json:"title"`
-								Note struct {
-									S string `json:"S"`
-								} `json:"note"`
-								Id struct {
-									S string `json:"S"`
-								} `json:"id"`
-							} `json:"M"`
-						} `json:"L"`
-					} `json:"notes"`
-					Scores struct {
-						L []struct {
-							M struct {
-								Title struct {
-									S string `json:"S"`
-								} `json:"title"`
-								Score struct {
-									N string `json:"N"`
-								} `json:"score"`
-								Id struct {
-									S string `json:"S"`
-								} `json:"id"`
-							} `json:"M"`
-						} `json:"L"`
-					} `json:"scores"`
-				} `json:"M"`
-			} `json:"L"`
-		} `json:"house_entries"`
-	} `json:"Item"`
-}
 
 var sess = session.Must(session.NewSession())
 
@@ -98,30 +48,9 @@ func HandleRequest(event *events.APIGatewayV2HTTPRequest) (*events.APIGatewayV2H
 		}, nil
 	}
 
-	var response Response
-
-	t, _ := json.Marshal(response)
-	err = json.Unmarshal([]byte(out.GoString()), &response)
-	if err != nil {
-		fmt.Println("error unmarshaling response:", err)
-		return &events.APIGatewayV2HTTPResponse{
-			StatusCode: 500,
-			Body:       fmt.Sprintf("failed to unmarshal response: %v\n%s\n%v\n", err, t, out.GoString()),
-		}, nil
-	}
-
-	responseJSON, err := json.Marshal(response)
-	if err != nil {
-		fmt.Println("Error marshaling response:", err)
-		return &events.APIGatewayV2HTTPResponse{
-			StatusCode: 500,
-			Body:       fmt.Sprintf("Failed to marshal response: %v", err),
-		}, nil
-	}
-
 	return &events.APIGatewayV2HTTPResponse{
 		StatusCode: 200,
-		Body:       string(responseJSON),
+		Body:       string(out.GoString()),
 	}, nil
 
 }
