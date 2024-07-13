58c7f12b1c42f6ac67e09d3ae5cd66cd6fcf1d32
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-07-13 12:50:33 -0400

    saving house page

---
+++ frontend/src/utils/modalStyle.js
@@ -0,0 +1,15 @@
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '90%',
+  maxWidth: 600,
+  maxHeight: '90vh',
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+  overflowY: 'auto',
+};
+
+export default style;
